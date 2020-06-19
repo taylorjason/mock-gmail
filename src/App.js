be5c5aefd4c94cde2f7ce157ad635028723051a3
Jason Taylor <taylorjason@users.noreply.github.com>
2020-06-19 10:43:52 -0400

    initial app layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import NavBar from './NavBar';
 import './App.css';
+import { EmailContext } from './EmailContext';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      detailsView: false,
+      listView: false,
+      composeView: false,
+      listViewEmails: []
+    };
+  }
+  handleChange(event) {
+    this.setState({
+      currentSearch: {
+        name: event.target.value
+      }
+    });
+  }
+
+  handleSearch(event) {
+    console.log('currentSearch: ', this.state.currentSearch.name);
+    fetch('https://pokeapi.co/api/v2/pokemon/' + this.state.currentSearch.name)
+      .then((response) => response.json()) // turn the response into json
+      .then((json) => {
+        this.setState({
+          currentPokemon: json || { name: this.state.currentSearch.name } //#REFACTOR: Use catch instead
+        });
+      })
+      .then(() => this.setState({ render: 'Search' }));
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div className="App">
+        <EmailContext.Provider
+          value={{
+            state: this.state,
+            handleChange: this.handleChange.bind(this),
+            handleSearch: this.handleSearch.bind(this)
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <header className="">
+            <NavBar />
+          </header>
+          <div className="main">
+            <h1> This is the main body area</h1>
+          </div>
+        </EmailContext.Provider>
+      </div>
+    );
+  }
 }
 
 export default App;
